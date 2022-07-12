04703bc3e6768c5e556ecd0df77ca1193c36db8a
Tim Mouskhelichvili <t.mouskhelichvili@protonmail.com>
2022-07-12 10:50:50 -0400

    Added more tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,13 @@
-import { AnatomicLocationLibrary } from 'src';
+import { AnatomicLocationLibrary, BodyPartIndex } from 'src';
 
 describe('AnatomicLocationLibrary', () => {
 
-    it('returns the correct version', () => {
+    it('returns the correct library version', () => {
         expect(new AnatomicLocationLibrary().getVersion()).toBe('testVersion');
+    });
+
+    it ('exports the BodyPartIndex correctly', () => {
+        expect(BodyPartIndex).toEqual(BodyPartIndex);
     });
 
 });
